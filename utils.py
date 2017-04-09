9c3667810fa29b1aad8f5d17468d2a7fa2625ebb
Pedro Baracho <pedropbaracho@gmail.com>
2017-04-09 02:53:30 -0300

    Added utils

---
+++ utils.py
@@ -0,0 +1,30 @@
+def memoize(f):
+    """Memoize function to cache results from function f"""
+    results = {}
+    def helper(n):
+        if n not in results:
+            results[n] = f(n)
+        return results[n]
+    return helper
+
+def load_pickle(filename, expected_size):
+    """Load pickle file. Returns the object loaded or None if failed"""
+
+    pickled_data = None
+    if os.path.isfile(filename):
+        try:
+            with open(filename, 'r') as f:
+                pickled_data = pickle.load(f)
+            if len(pickled_data) != expected_size:
+                os.remove(filename)
+                pickled_data = None
+        except:
+            os.remove(WORD_COUNT_PICKLE)
+            pickled_data = None
+
+    return pickled_data
+
+def save_pickle(obj, filename):
+    """Serializes obj on filename"""
+    with open(filename, 'w') as f:
+        pickle.dump(obj, f)
