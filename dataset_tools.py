6c63e4c5d7f9bdeb6a7205bb2a5d03ed9b0a24e9
Pedro Baracho <pedropbaracho@gmail.com>
2017-04-09 02:53:08 -0300

    Added dataset tools

---
+++ dataset_tools.py
@@ -0,0 +1,43 @@
+import os, sys
+from six.moves.urllib.request import urlretrieve
+import zipfile
+
+DICT_FILE_URL = 'http://mattmahoney.net/dc/'
+DICT_FILE = os.path.join(os.path.dirname((os.path.realpath(__file__))), "text8.zip")
+DICT_FILE_SIZE = 31344016
+last_percent_reported = None
+
+def read_dict_file(filename=DICT_FILE):
+    with zipfile.ZipFile(filename) as f:
+        name = f.namelist()[0]
+        data = f.read(name)
+    return data
+
+def download_progress_hook(count, blockSize, totalSize):
+    """A hook to report the progress of a download. Reports every 5% change in download progress."""
+    global last_percent_reported
+    percent = int(count * blockSize * 100 / totalSize)
+
+    if last_percent_reported != percent:
+        if percent % 5 == 0:
+            sys.stdout.write("%s%%" % percent)
+            sys.stdout.flush()
+        else:
+            sys.stdout.write(".")
+            sys.stdout.flush()
+        
+        last_percent_reported = percent
+
+def maybe_download(filename=DICT_FILE, expected_bytes=DICT_FILE_SIZE):
+    """Download a file if not present, and make sure it's the right size."""
+    if not os.path.exists(filename):
+        print("Downloading dictionary file")
+        filename, _ = urlretrieve(DICT_FILE_URL + filename, filename, reporthook=download_progress_hook)
+    statinfo = os.stat(filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified dictionary file')
+    else:
+        raise Exception(
+        'Failed to verify ' + filename + '. Can you get to it with a browser?')
+    
+    return filename
